10362f1812f77fedfc9c8ee80535d1321deb9c83
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-10-19 15:58:30 -0600

    Add project files.

---
+++ StudentInfo/Pages/Graphs.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StudentInfo.Pages.Shared
+{
+    public class GraphsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
