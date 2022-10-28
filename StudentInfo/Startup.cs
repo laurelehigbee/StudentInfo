4418fb9538e6b772eb64bd054123e63c2301bedd
madisonpitts <madpitts@byu.edu>
2022-10-27 21:35:16 -0600

    connecting to database

--- StudentInfo/Startup.cs
+++ StudentInfo/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using StudentInfo.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,10 @@ namespace StudentInfo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddDbContext<EmployeesDbContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:EmployeesDbConnection"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
