d820761b218c1d7c78740cc8e67b65307b350f3a
laurelehigbee <90651214+laurelehigbee@users.noreply.github.com>
2022-10-29 11:01:42 -0600

    Controllers created/updated

---
+++ StudentInfo/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using StudentInfo.Models;
+using System.Diagnostics;
+
+namespace StudentInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
